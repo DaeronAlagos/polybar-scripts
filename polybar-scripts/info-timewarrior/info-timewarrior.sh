f3b933e2a475b326ae1af201048dba8e07f40b69
James Curtis <mybastion@gmail.com>
2020-05-07 13:11:11 +0100

    Fix empty array error

--- polybar-scripts/info-timewarrior/info-timewarrior.sh
+++ polybar-scripts/info-timewarrior/info-timewarrior.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
-activity=$(timew export from $(date +%Y-%m-%d) | jq '.[-1]')
-end=$(echo "$activity" | jq -er '.end')
+activity_list=$(timew export from $(date +%Y-%m-%d) | jq '.')
 
 get_activity() {
   start=$(date -u -d "$(echo $activity | jq -r '.start' | sed 's,\([0-9]\{4\}\)\([0-9]\{2\}\)\([0-9]\{2\}\)\(T\)\([0-9]\{2\}\)\([0-9]\{2\}\)\([0-9]\{2\}\)\(Z\),\1-\2-\3 \5:\6:\7,')" +"%s")
@@ -11,10 +10,18 @@ get_activity() {
   echo $tags "|" $diff
 }
 
-if [ "$end" = null ]; then
-  get_activity
-else
-  echo 'Not Tracking Time'
-fi
+get_state() {
+  activity=$(echo "$activity_list" | jq '.[-1]')
+  end=$(echo "$activity" | jq -er '.end')
+  if [ "$end" = null ]; then
+    get_activity
+  else
+    echo 'Not Tracking Time'
+  fi
+}
 
-# TODO: If jq array is empty then error occurs
+if [ $(echo "$activity_list" | jq '. | length') = 0 ]; then
+  echo 'Not Tracking Time'
+else
+  get_state
+fi
